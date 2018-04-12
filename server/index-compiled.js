4b0085921ed0482143ef0e1e5c1ec5b1c85079d5
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-13 02:53:36 +0400

    added cors module

--- server/index-compiled.js
+++ server/index-compiled.js
@@ -4,6 +4,12 @@ var _util = require('./util.js');
 
 var utils = _interopRequireWildcard(_util);
 
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 var express = require('express');
@@ -16,25 +22,26 @@ var app = express();
 
 app.use(helmet());
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+// 	next();
+// });
+
 
 app.use('/images/', express.static(join(__dirname, '../client/images/')));
 app.use('/css/', express.static(join(__dirname, '../client/css/')));
 app.use('/js/', express.static(join(__dirname, '../client/js/')));
 
-app.get('/', function (req, res) {
+app.get('/', (0, _cors2.default)(), function (req, res) {
 	res.sendFile(join(__dirname, '../client/index.html'));
 });
 
-app.get('/play', function (req, res) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+app.get('/play', (0, _cors2.default)(), function (req, res) {
+	// res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	// res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 	var numbers = utils.generateRandoms();
 	var bonus = utils.generateBonus(15);
 	res.status(200).json({
--- server/index.js
+++ server/index.js
@@ -2,31 +2,33 @@ var express = require('express');
 var join = require('path').join;
 var helmet = require('helmet');
 import * as utils from './util.js';
+import cors from 'cors';
 
 var port =  process.env.PORT || 3000;
 var app = express();
 
 app.use(helmet());
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+// 	next();
+// });
+
 
 app.use('/images/', express.static(join(__dirname, '../client/images/')));
 app.use('/css/', express.static(join(__dirname, '../client/css/')));
 app.use('/js/', express.static(join(__dirname, '../client/js/')));
 
-app.get('/', function(req, res) {
+app.get('/', cors(), function(req, res) {
 	res.sendFile(join(__dirname, '../client/index.html'));
 });
 
-app.get('/play', function(req, res) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+app.get('/play', cors(), function(req, res) {
+	// res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	// res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 	const numbers = utils.generateRandoms();
 	const bonus = utils.generateBonus(15);
 	res.status(200).json({
